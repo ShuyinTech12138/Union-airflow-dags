ab6809e636f55c9391021b8bb016aa4fc6fb510c
Shuyin Zheng <szheng@local28union.com>
2026-06-15 13:14:39 -0400

    fix unused imports in biweekly_hours_report

--- biweekly_hours_report.py
+++ biweekly_hours_report.py
@@ -1,7 +1,6 @@
 from airflow import DAG
 from airflow.providers.microsoft.mssql.hooks.mssql import MsSqlHook
-from airflow.providers.standard.operators.python import PythonOperator
-from datetime import datetime, timedelta
+from datetime import datetime
 import pandas as pd
 from airflow.operators.python import PythonOperator, ShortCircuitOperator
 import smtplib
